0acdb57327ac5b6a25ef8dfdf2d427a9f59a0606
김주래 <rlawnfo5824@douzone.com>
2024-07-22 17:45:01 +0900

    초기 커밋.

---
+++ WpfAppCalc/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppCalc
+{
+    public partial class MainWindowViewModel: ObservableObject
+    {
+        [ObservableProperty] int? operand1 = null;
+        [ObservableProperty] int? operand2 = null;
+        [ObservableProperty] string? mathOperator = null;
+
+        private string[] _operators = new string[] { "+", "-", "*", "/" };
+
+        public MainWindowViewModel()
+        {
+            
+        }
+
+        [RelayCommand]
+        private void Calculate()
+        {
+
+        }
+    }
+}
